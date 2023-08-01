7cad2035d0341cc25a5ec049a93027f0929e98b3
Karlis Veckagans <veckaganskarlis@gmail.com>
2023-08-01 18:24:42 +0300

    app deployed!!

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 
 import App from './App.vue'
 import BaseCard from './components/UI/BaseCard'
 import BaseButton from './components/UI/BaseButton'
 import BaseBadge from './components/UI/BaseBadge'
 import BaseSpinner from './components/UI/BaseSpinner'
-import BaseDialog from './components/UI/BaseDialog'
 import store from './store';
 import router from './router';
+
+// defineAsyncComponent downloads and loads a components only when it is needed
+// so it helps with performance
+const BaseDialog = defineAsyncComponent(() => import('./components/UI/BaseDialog'))
 
 const app = createApp(App)
 
